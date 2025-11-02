77c8fc16c6b67fb881673f150c0ff5ecc4fdece6
luissoy <luisserranomarin@gmail.com>
2025-11-02 14:28:41 +0100

    chore: initialize Spring Boot project

---
+++ src/main/java/com/luissoy/historicalprices/HistoricalPricesApplication.java
@@ -0,0 +1,13 @@
+package com.luissoy.historicalprices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HistoricalPricesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HistoricalPricesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luissoy/historicalprices/HistoricalPricesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luissoy.historicalprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HistoricalPricesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
