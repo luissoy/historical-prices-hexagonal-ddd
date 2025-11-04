ac8d718f068d9bd7d719e1c564efe42f7f984834
luissoy <luisserranomarin@gmail.com>
2025-11-04 14:03:05 +0100

    feat: add domain not found exception identifier constructor

--- Domain/src/main/java/com/luissoy/historicalprices/domain/price/exception/PriceNotFoundException.java
+++ Domain/src/main/java/com/luissoy/historicalprices/domain/price/exception/PriceNotFoundException.java
@@ -1,9 +1,15 @@
 package com.luissoy.historicalprices.domain.price.exception;
 
+import com.luissoy.historicalprices.domain.price.valueobject.PriceId;
+import com.luissoy.historicalprices.domain.product.valueobject.ProductId;
 import com.luissoy.historicalprices.domain.shared.exception.NotFoundException;
 
 public class PriceNotFoundException extends NotFoundException {
-    public PriceNotFoundException(Long id) {
-        super("Price with id " + id + " not found");
+    public PriceNotFoundException(PriceId id) {
+        super("Price with id " + id.toString() + " not found");
+    }
+
+    public PriceNotFoundException(ProductId id) {
+        super("Price for product with id " + id.toString() + " not found");
     }
 }
--- Domain/src/main/java/com/luissoy/historicalprices/domain/product/exception/ProductNotFoundException.java
+++ Domain/src/main/java/com/luissoy/historicalprices/domain/product/exception/ProductNotFoundException.java
@@ -1,9 +1,10 @@
 package com.luissoy.historicalprices.domain.product.exception;
 
+import com.luissoy.historicalprices.domain.product.valueobject.ProductId;
 import com.luissoy.historicalprices.domain.shared.exception.NotFoundException;
 
 public class ProductNotFoundException extends NotFoundException {
-    public ProductNotFoundException(Long id) {
-        super("Product with id " + id + " not found");
+    public ProductNotFoundException(ProductId id) {
+        super("Product with id " + id.toString() + " not found");
     }
 }
