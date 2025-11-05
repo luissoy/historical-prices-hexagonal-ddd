32b2a865c954bdd3b6f08e8f400b5881f2a9a6d9
luissoy <luisserranomarin@gmail.com>
2025-11-05 17:54:08 +0100

    feat: add pom update

--- Application/src/main/java/com/luissoy/historicalprices/application/price/port/out/SavePricePort.java
+++
@@ -1,7 +0,0 @@
-package com.luissoy.historicalprices.application.price.port.out;
-
-import com.luissoy.historicalprices.domain.price.Price;
-
-public interface SavePricePort {
-    Price save(Price price);
-}
--- Application/src/main/java/com/luissoy/historicalprices/application/product/port/out/SaveProductPort.java
+++
@@ -1,7 +0,0 @@
-package com.luissoy.historicalprices.application.product.port.out;
-
-import com.luissoy.historicalprices.domain.product.Product;
-
-public interface SaveProductPort {
-    Product save(Product product);
-}
