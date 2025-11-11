ac67677bcbbe41601627c8a602271a5fff060846
luissoy <luisserranomarin@gmail.com>
2025-11-11 15:34:02 +0100

    feat: add infrastructure out persistence refactor

--- infrastructure/src/main/java/com/luissoy/historicalprices/infrastructure/out/jpa/Product/JpaProductRepository.java
+++
@@ -1,8 +0,0 @@
-package com.luissoy.historicalprices.infrastructure.out.jpa.Product;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JpaProductRepository extends JpaRepository<ProductEntity, Long> {
-}
--- infrastructure/src/main/java/com/luissoy/historicalprices/infrastructure/out/persistence/product/ProductEntityMapper.java
+++ infrastructure/src/main/java/com/luissoy/historicalprices/infrastructure/out/persistence/product/ProductEntityMapper.java
@@ -1,8 +1,11 @@
-package com.luissoy.historicalprices.infrastructure.out.jpa.Product;
+package com.luissoy.historicalprices.infrastructure.out.persistence.product;
 
 import com.luissoy.historicalprices.domain.product.Product;
 import com.luissoy.historicalprices.domain.product.valueobject.*;
 import org.springframework.stereotype.Component;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 @Component
 public class ProductEntityMapper {
@@ -17,9 +20,18 @@ public class ProductEntityMapper {
 
     public Product toDomain(ProductEntity entity) {
         return new Product(
-                new ProductId(entity.getId()),
-                new ProductName(entity.getName()),
-                new ProductDescription(entity.getDescription())
+                new ProductId(entity.id()),
+                new ProductName(entity.name()),
+                new ProductDescription(entity.description())
+        );
+    }
+
+
+    public Product resultSetToDomain(ResultSet rs) throws SQLException {
+        return new Product(
+                new ProductId(rs.getLong("id")),
+                new ProductName(rs.getString("name")),
+                new ProductDescription(rs.getString("description"))
         );
     }
 }
