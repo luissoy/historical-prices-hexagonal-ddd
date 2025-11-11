af40cf945abee00e0fe432eabd92a2288d5c34ec
luissoy <luisserranomarin@gmail.com>
2025-11-11 17:17:44 +0100

    feat: add infrastructure bean config and pom

--- infrastructure/src/main/java/com/luissoy/historicalprices/infrastructure/config/BeanConfig.java
+++ infrastructure/src/main/java/com/luissoy/historicalprices/infrastructure/config/BeanConfig.java
@@ -1,21 +1,26 @@
 package com.luissoy.historicalprices.infrastructure.config;
 
 import com.fasterxml.jackson.databind.Module;
-import com.luissoy.historicalprices.application.price.PriceService;
-import com.luissoy.historicalprices.application.price.mapper.PriceMapper;
-import com.luissoy.historicalprices.application.price.port.in.PriceUseCase;
-import com.luissoy.historicalprices.application.product.ProductService;
-import com.luissoy.historicalprices.application.product.mapper.ProductMapper;
-import com.luissoy.historicalprices.application.product.port.in.ProductUseCase;
+import com.luissoy.historicalprices.application.price.GetProductPriceHistoryUseCase;
+import com.luissoy.historicalprices.application.price.AddProductPriceUseCase;
+import com.luissoy.historicalprices.application.price.GetProductActivePriceUseCase;
+import com.luissoy.historicalprices.application.price.PriceMapper;
+import com.luissoy.historicalprices.application.product.CreateProductUseCase;
+import com.luissoy.historicalprices.application.product.GetProductUseCase;
+import com.luissoy.historicalprices.application.product.ProductMapper;
 import com.luissoy.historicalprices.domain.price.PriceRepository;
 import com.luissoy.historicalprices.domain.product.ProductRepository;
 import com.luissoy.historicalprices.infrastructure.in.rest.mapper.PriceApiMapper;
 import com.luissoy.historicalprices.infrastructure.in.rest.mapper.ProductApiMapper;
-import com.luissoy.historicalprices.infrastructure.out.jpa.Price.PriceEntityMapper;
-import com.luissoy.historicalprices.infrastructure.out.jpa.Product.ProductEntityMapper;
+import com.luissoy.historicalprices.infrastructure.out.persistence.price.PriceEntityMapper;
+import com.luissoy.historicalprices.infrastructure.out.persistence.price.PriceRepositoryAdapter;
+import com.luissoy.historicalprices.infrastructure.out.persistence.product.ProductDataAccess;
+import com.luissoy.historicalprices.infrastructure.out.persistence.product.ProductEntityMapper;
 import org.openapitools.jackson.nullable.JsonNullableModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 
 @Configuration
 public class BeanConfig {
@@ -51,25 +56,75 @@ public class BeanConfig {
     }
 
     @Bean
-    public ProductUseCase productUseCase(
+    public Module jsonNullableModule() {
+        return new JsonNullableModule();
+    }
+
+    @Bean
+    public CreateProductUseCase createProductUseCase(
             ProductRepository productRepository,
             ProductMapper productMapper
     ) {
-        return new ProductService(productRepository, productMapper);
+        return new CreateProductUseCase(productRepository, productMapper);
     }
 
     @Bean
-    public PriceUseCase priceUseCase(
-            PriceRepository priceRepository,
+    public GetProductUseCase getProductUseCase(
+            ProductRepository productRepository,
+            ProductMapper productMapper
+    ) {
+        return new GetProductUseCase(productRepository, productMapper);
+    }
+
+    @Bean
+    public AddProductPriceUseCase addPriceUseCase(
             ProductRepository productRepository,
             PriceMapper priceMapper
     ) {
-        return new PriceService(priceRepository, productRepository, priceMapper);
+        return new AddProductPriceUseCase(productRepository, priceMapper);
     }
 
+    @Bean
+    public GetProductActivePriceUseCase getActivePriceUseCase(
+            PriceRepository priceRepository,
+            PriceMapper priceMapper
+    ) {
+        return new GetProductActivePriceUseCase(priceRepository, priceMapper);
+    }
 
     @Bean
-    public Module jsonNullableModule() {
-        return new JsonNullableModule();
+    public GetProductPriceHistoryUseCase getPriceHistoryUseCase(
+            ProductRepository productRepository,
+            PriceMapper priceMapper
+    ) {
+        return new GetProductPriceHistoryUseCase(productRepository, priceMapper);
+    }
+
+    @Bean
+    public ProductDataAccess productDataAccess(
+            JdbcTemplate jdbc,
+            ProductEntityMapper productEntityMapper
+    ) {
+        return new ProductDataAccess(
+                jdbc,
+                productEntityMapper,
+                new SimpleJdbcInsert(jdbc)
+                        .withTableName("PRODUCTS")
+                        .usingGeneratedKeyColumns("id")
+        );
+    }
+
+    @Bean
+    public PriceRepositoryAdapter priceRepositoryAdapter(
+            JdbcTemplate jdbc,
+            PriceEntityMapper priceEntityMapper
+    ) {
+        return new PriceRepositoryAdapter(
+                jdbc,
+                priceEntityMapper,
+                new SimpleJdbcInsert(jdbc)
+                        .withTableName("prices")
+                        .usingGeneratedKeyColumns("id")
+        );
     }
 }
